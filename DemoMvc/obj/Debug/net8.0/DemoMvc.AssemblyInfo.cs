32c30b480c4f7e1edca4c69f3a58cca981e390bc
Lynk229 <nguyenly10072002@gmail.com>
2024-09-20 15:45:36 +0700

    createproject

--- DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
+++ DemoMvc/obj/Debug/net8.0/DemoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5004b1ef98689019958a9111a25613ad4876bf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfea8b2476d6e572ae483ae1874793ee7dc2ad09")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
